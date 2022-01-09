535d3481e2e3e7e2364e5f5459825f655c4c8dcf
Дерун Олег <mortallylive@ya.ru>
2022-01-09 03:50:24 +0300

    home

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'catalog/index.html', data)
