8f602da508458afd70f86fb8bebf1e0fc8e48e69
MonteNolen <gavalost@ukr.net>
2022-01-10 04:00:29 +0300

    work

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,28 @@ from django.db import models
 from django.urls import reverse
 import uuid # Требуется для уникальных экземпляров книг
 
+class Author(models.Model):
+    """
+    Модель представляющая автора.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Умер', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Возвращает url для доступа к определенному экземпляру автора.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+
+    def __str__(self):
+        """
+        Строка представляющая модель объекта.
+        """
+        return '%s, %s' % (self.last_name, self.first_name)
+
 class Genre(models.Model):
     """
     Модель представляющая жанр книги (e.g. Science Fiction, Non Fiction).
@@ -70,24 +92,3 @@ class BookInstance(models.Model):
         """
         return '{0} ({1})'.format (self.id, self.book.title)  # '%s (%s)' % (self.id,self.book.title)
 
-class Author(models.Model):
-    """
-    Модель представляющая автора.
-    """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Умер', null=True, blank=True)
-
-    def get_absolute_url(self):
-        """
-        Возвращает url для доступа к определенному экземпляру автора.
-        """
-        return reverse('author-detail', args=[str(self.id)])
-
-
-    def __str__(self):
-        """
-        Строка представляющая модель объекта.
-        """
-        return '%s, %s' % (self.last_name, self.first_name)
