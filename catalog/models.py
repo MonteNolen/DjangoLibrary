8e660519c60ed62d031b3d316a44d66644e5a48b
Дерун Олег <mortallylive@ya.ru>
2022-01-12 11:30:18 +0300

    home

--- catalog/models.py
+++ catalog/models.py
@@ -63,6 +63,13 @@ class Book(models.Model):
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
     """
@@ -91,3 +98,8 @@ class BookInstance(models.Model):
         Строка для представления объекта модели
         """
         return '{0} ({1})'.format (self.id, self.book.title)  # '%s (%s)' % (self.id,self.book.title)
+
+
+
+########################################################################
+
